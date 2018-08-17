8b02f05a6678253cf29b11946f8b0166e0e911ee
willi <willi@Williana.local>
2018-08-17 15:00:02 -0300

    Atualização de uma classe modelo

--- src/Models/Funcionario.java
+++ src/Models/Funcionario.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Models;
 
 /**
@@ -10,5 +5,53 @@ package Models;
  * @author willi
  */
 public class Funcionario {
+  
+    private int id;
+    private String nome;
+    private String contato;
+    private String login;
+    private String senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
     
 }
